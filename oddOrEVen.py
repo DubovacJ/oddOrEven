fd539e68c8b0f52ffd742441f71cd618c757500f
JovanD <58370468+DubovacJ@users.noreply.github.com>
2023-12-13 19:13:28 +0100

    first commit

---
+++ oddOrEVen.py
@@ -0,0 +1,10 @@
+while True:
+    number = input("input any number ")
+    if number.isdigit():
+        print("good job moron you have entered the number")
+        if int(number)%2 == 0:
+            print("this is an even number")
+        else:
+            print("this is a odd number")
+    else:
+        print("this is not a number, please input a number")
